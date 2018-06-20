1e7f56ee4ac662d83dda33cb247e0218ef1d12f5
Nsteel <nicoacero94@gmail.com>
2018-06-20 17:45:12 +0200

    Added possibility of selecting metamodels in the new project wizard
    - The user can now select the metamodels for source and target in the NewIntegrationProjectWizard. If this is not wanted, it is still possible to create a project with a blank schema (no imports).
    - The URIs of the selected metamodels are resolved and written in the new project's schema.

--- org.emoflon.ibex.tgg.editor.ui/src/org/moflon/tgg/mosl/ui/wizards/NewIntegrationWizard.java
+++ org.emoflon.ibex.tgg.editor.ui/src/org/moflon/tgg/mosl/ui/wizards/NewIntegrationWizard.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
@@ -22,11 +23,11 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
-import org.eclipse.emf.ecore.presentation.EcoreActionBarContributor.ExtendedLoadResourceAction.TargetPlatformPackageDialog;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.ui.INewWizard;
 import org.emoflon.ibex.tgg.ide.admin.IbexTGGNature;
 import org.moflon.core.utilities.LogUtils;
@@ -34,9 +35,14 @@ import org.moflon.core.utilities.WorkspaceHelper;
 import org.moflon.tgg.mosl.defaults.AttrCondDefLibraryProvider;
 import org.moflon.tgg.mosl.defaults.DefaultFilesHelper;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
 public class NewIntegrationWizard extends AbstractMoflonWizard implements INewWizard {
 	protected AbstractMoflonProjectInfoPage projectInfo;
-	protected List<String> importURIs;
+	protected AbstractMoflonMetaModelSelectionPage srcSelection;
+	protected AbstractMoflonMetaModelSelectionPage trgSelection;
+	protected Set<String> importURIs;
 	protected List<String> sourceMetamodelsNames;
 	protected List<String> targetMetamodelsNames;
 
@@ -46,13 +52,26 @@ public class NewIntegrationWizard extends AbstractMoflonWizard implements INewWi
 
 	@Override
 	public void addPages() {
-		importURIs = new ArrayList<String>();
+		importURIs = Sets.newHashSet();
 		sourceMetamodelsNames = new ArrayList<String>();
 		targetMetamodelsNames = new ArrayList<String>();
-		importURIs.addAll(getMetamodelURIs("Select source metamodels:", true));
-		importURIs.addAll(getMetamodelURIs("Select target metamodels:", false));
 		projectInfo = new NewIntegrationProjectInfoPage();
+		srcSelection = new NewIntegrationProjectSrcModelSelectionPage();
+		trgSelection = new NewIntegrationProjectTrgModelSelectionPage();
 		addPage(projectInfo);
+		addPage(srcSelection);
+		addPage(trgSelection);
+	}
+
+	@Override
+	public IWizardPage getNextPage(IWizardPage currentPage) {
+		if (currentPage == projectInfo && projectInfo.isSelectMetaModels()) {
+			return srcSelection;
+		} else if (currentPage == srcSelection) {
+			return trgSelection;
+		}
+
+		return null;
 	}
 
 	protected void createProject(IProgressMonitor monitor, IProject project) throws CoreException {
@@ -69,8 +88,8 @@ public class NewIntegrationWizard extends AbstractMoflonWizard implements INewWi
 	protected void generateDefaultFiles(final IProgressMonitor monitor, IProject project) throws CoreException {
 		try {
 			final SubMonitor subMon = SubMonitor.convert(monitor, "Generating default files", 3);
-			String defaultSchema = DefaultFilesHelper.generateDefaultSchema(project.getName(), importURIs,
-					sourceMetamodelsNames, targetMetamodelsNames);
+			String defaultSchema = DefaultFilesHelper.generateDefaultSchema(project.getName(),
+					Lists.newArrayList(importURIs), sourceMetamodelsNames, targetMetamodelsNames);
 			IPath pathToSchema = new Path(IbexTGGNature.SCHEMA_FILE);
 			WorkspaceHelper.addAllFoldersAndFile(project, pathToSchema, defaultSchema, subMon.split(1));
 			WorkspaceHelper.addAllFolders(project, "src/org/emoflon/ibex/tgg/rules", subMon.split(1));
@@ -81,24 +100,36 @@ public class NewIntegrationWizard extends AbstractMoflonWizard implements INewWi
 		}
 	}
 
-	private List<String> getMetamodelURIs(final String dialogMessage, boolean isSource) {
-		final TargetPlatformPackageDialog selectPackageDialog = new TargetPlatformPackageDialog(getShell());
-		selectPackageDialog.setMessage(dialogMessage);
-		selectPackageDialog.open();
-		Object[] selectedPackages = selectPackageDialog.getResult();
-		List<String> uris = new ArrayList<String>();
+	private void getMetamodelURIs(IProgressMonitor monitor, Object[] selectedPackages, boolean isSource) {
+		final SubMonitor subMon = SubMonitor.convert(monitor, "Getting metamodel URIs", 3);
 		if (selectedPackages != null) {
 			List<?> nsURIs = Arrays.asList(selectedPackages);
 			ResourceSet resourceSet = new ResourceSetImpl();
 			resourceSet.getURIConverter().getURIMap().putAll(EcorePlugin.computePlatformURIMap(true));
 
-			Map<String, URI> ePackageNsURItoGenModelLocationMap = EcorePlugin
+			Map<String, URI> ePackageNsURItoGenModelLocationMapEnv = EcorePlugin
+					.getEPackageNsURIToGenModelLocationMap(false);
+			Map<String, URI> ePackageNsURItoGenModelLocationMapTrg = EcorePlugin
 					.getEPackageNsURIToGenModelLocationMap(true);
 			for (int i = 0, length = selectedPackages.length; i < length; i++) {
-				URI location = ePackageNsURItoGenModelLocationMap.get(selectedPackages[i]);
-				Resource resource = resourceSet.getResource(location, true);
-				EcoreUtil.resolveAll(resource);
+				URI location = ePackageNsURItoGenModelLocationMapEnv.get(selectedPackages[i]);
+				if (location == null) {
+					location = ePackageNsURItoGenModelLocationMapTrg.get(selectedPackages[i]);
+				}
+				if (location == null) {
+					LogUtils.error(logger, "Package with URI %s not found", selectedPackages[i].toString());
+					continue;
+				}
+				try {
+					Resource resource = resourceSet.getResource(location, true);
+					EcoreUtil.resolveAll(resource);
+				} catch (RuntimeException e) {
+					LogUtils.error(logger, "Error while loading resource with URI: %s", location.toString());
+				}
+
 			}
+
+			subMon.worked(1);
 
 			List<Resource> resources = resourceSet.getResources();
 
@@ -109,13 +140,16 @@ public class NewIntegrationWizard extends AbstractMoflonWizard implements INewWi
 							sourceMetamodelsNames.add(ePackage.getName());
 						else
 							targetMetamodelsNames.add(ePackage.getName());
-						uris.add(resource.getURI().toString());
+						importURIs.add(resource.getURI().toString());
 						break;
 					}
 				}
 			}
+
+			subMon.worked(2);
 		}
-		return uris;
+
+		subMon.setWorkRemaining(0);
 	}
 
 	private Collection<EPackage> getAllPackages(Resource resource) {
@@ -140,11 +174,13 @@ public class NewIntegrationWizard extends AbstractMoflonWizard implements INewWi
 	@Override
 	protected void doFinish(final IProgressMonitor monitor) throws CoreException {
 		try {
-			final SubMonitor subMon = SubMonitor.convert(monitor, "Creating eMoflon::Ibex project", 6);
+			final SubMonitor subMon = SubMonitor.convert(monitor, "Creating eMoflon::Ibex project", 12);
 
 			String projectName = projectInfo.getProjectName();
 			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
 			createProject(subMon.split(3), project);
+			getMetamodelURIs(subMon.split(3), srcSelection.getSelectedMetaModels(), true);
+			getMetamodelURIs(subMon.split(3), trgSelection.getSelectedMetaModels(), false);
 			generateDefaultFiles(subMon.split(3), project);
 		} catch (Exception e) {
 			LogUtils.error(logger, e);
