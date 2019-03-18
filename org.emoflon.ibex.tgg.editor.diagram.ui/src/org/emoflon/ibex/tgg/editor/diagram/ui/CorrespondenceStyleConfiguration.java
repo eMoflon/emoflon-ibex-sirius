12511099752832ca798060376060cc956dae7ea4
Lars <lars.fritsche@es.tu-darmstadt.de>
2019-03-18 13:09:00 +0100

    fixed a problem that occured due to package renaming in the eclipse-sirius

--- org.emoflon.ibex.tgg.editor.diagram.ui/src/org/emoflon/ibex/tgg/editor/diagram/ui/CorrespondenceStyleConfiguration.java
+++ org.emoflon.ibex.tgg.editor.diagram.ui/src/org/emoflon/ibex/tgg/editor/diagram/ui/CorrespondenceStyleConfiguration.java
@@ -1,7 +1,7 @@
 package org.emoflon.ibex.tgg.editor.diagram.ui;
 
-import org.eclipse.sirius.diagram.ui.tools.api.figure.anchor.AnchorProvider;
 import org.eclipse.sirius.diagram.ui.tools.api.graphical.edit.styles.SimpleStyleConfiguration;
+import org.eclipse.sirius.ext.gmf.runtime.gef.ui.figures.util.AnchorProvider;
 
 public class CorrespondenceStyleConfiguration extends SimpleStyleConfiguration {
 	private static CorrespondenceStyleConfiguration instance = new CorrespondenceStyleConfiguration();
